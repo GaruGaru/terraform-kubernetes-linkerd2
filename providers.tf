2191f9b459e5c555757976ed3bd329975bad1d8f
GaruGaru <garuglieritommaso.dev@gmail.com>
2021-03-14 15:06:46 +0100

    update resource definitions for cert-manager.io/v1

--- providers.tf
+++ providers.tf
@@ -10,5 +10,4 @@ provider "kubernetes-alpha" {
   host                   = var.kubernetes.host
   cluster_ca_certificate = var.kubernetes.cluster_ca_certificate
   token                  = var.kubernetes.token
-  version = "2.0.2"
 }
